5d0de582b66cb7656325d5b270ad5334e7b9ba94
Pranay Mundra <pranaymundra4510@gmail.com>
2021-03-29 16:55:54 -0700

    new website

---
+++ assets/js/live-resume.js
@@ -0,0 +1,90 @@
+$('[data-toggle="collapsible-nav"]').on('click', function(e){
+    var target = ($(this).data('target'));
+    $('#' + target).toggleClass('show');
+});
+
+$(document).ready(function(){
+    if(window.innerWidth >= 992) {
+        $('#collapsible-nav').addClass('show');  //Show navigation menu in bigger screens by default.
+    } else {
+        $('#collapsible-nav').removeClass('show');
+    }
+
+    if ($('.hover-box').length) {
+        setHoverBoxPerspective();
+    }
+});
+
+$(window).resize(
+    function() {
+        if ($('.hover-box').length) {
+            setHoverBoxPerspective();
+        }
+    }
+);
+
+function setHoverBoxPerspective() {
+    $('.hover-box').css({
+        'perspective': function () {
+            return Math.max( $(this).width(), $(this).height() ) * 2 + 50;
+        }
+    });
+}
+
+
+var classNames = ['in-up', 'in-right', 'in-down', 'in-left', 'out-up', 'out-right', 'out-down', 'out-left']; // Animation classes.
+
+$('.hover-box').hover(
+    function (event) {
+        var direction = "up";
+        if(jQuery.fn.entry){ //Check if entry js file is loaded.
+            direction = $(this).entry({ e: event }); // Get mouse in direction.
+        }
+
+        $(this).removeClass(classNames.join(" ")); // Remove existing animation classes.
+        $(this).addClass("in-" + direction); //Add mouse in animation
+
+    }, 
+    
+    function (event) {
+
+        var direction = "up";
+        if(jQuery.fn.entry){
+            direction = $(this).entry({ e: event }); // Get mouse out direction.
+        }
+
+        $(this).removeClass(classNames.join(" "));
+        $(this).addClass("out-" + direction); //Add mouse out animation
+
+    }
+);
+
+function expandPaper1() {
+    var dots = document.getElementById("dots1");
+    var moreText = document.getElementById("more1");
+    var btnText = document.getElementById("readMore1");
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Read More";
+        moreText.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Read Less";
+        moreText.style.display = "inline";
+    }
+}
+
+function expandPaper2() {
+    var dots = document.getElementById("dots2");
+    var moreText = document.getElementById("more2");
+    var btnText = document.getElementById("readMore2");
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Read More";
+        moreText.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Read Less";
+        moreText.style.display = "inline";
+    }
+}
